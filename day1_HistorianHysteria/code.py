05162fabc518cb1b6f913f55a55f9a72f59fbd38
nlevasseur <nathan.levasseur@inssa-rouen.fr>
2024-12-03 02:01:49 +0100

    Day 1 puzzle-part2 succeeded

--- day1_HistorianHysteria/code.py
+++ day1_HistorianHysteria/code.py
@@ -13,6 +13,9 @@ with open("input.txt", "r") as file:
         leftList.append(int(line[:5]))
         rightList.append(int(line[8:]))
 
+'''Part One'''
+
+
 dist = []
 
 while leftList != []:
@@ -25,3 +28,27 @@ while leftList != []:
 DISTANCE = sum(dist)
 
 print("The total distance between the two lists is:", DISTANCE)
+
+
+'''Part Two'''
+
+with open("input.txt", "r") as file:
+    for line in file:
+        leftList.append(int(line[:5]))
+        rightList.append(int(line[8:]))
+
+def similarity(L1: list, L2: list) -> float:
+    '''
+    Entrées:
+        - L1, list: première liste
+        - L2, list: deuxième liste
+    Sortie:
+        - sim, float: similitude entre les deux listes
+    '''
+    sim = 0
+    for number in L1:
+        sim += number*L2.count(number)
+    return sim
+
+SIMILARITY = similarity(leftList, rightList)
+print("Their similarity score is:", SIMILARITY)
